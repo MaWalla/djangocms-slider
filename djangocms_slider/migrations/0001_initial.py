7de2e2c27a86069ee4c3a4db9c9f82973801263e
Max Walaschewski <mawalla@protonmail.com>
2022-07-12 23:07:34 +0200

    added now mandatory on_delete to migration fields

--- djangocms_slider/migrations/0001_initial.py
+++ djangocms_slider/migrations/0001_initial.py
@@ -14,11 +14,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Slide',
             fields=[
-                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
+                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin', on_delete=models.CASCADE)),
                 ('image', models.ImageField(upload_to=cms.models.pluginmodel.get_plugin_media_path, verbose_name='image')),
                 ('url', models.CharField(help_text='If present, clicking on image will take user to link.', max_length=255, null=True, verbose_name='link', blank=True)),
                 ('caption', models.TextField(help_text='Specifies text that occurs on the slide.', max_length=255, null=True, verbose_name='caption', blank=True)),
-                ('page_link', models.ForeignKey(blank=True, to='cms.Page', help_text='If present, clicking on image will take user to specified page.', null=True, verbose_name='page')),
+                ('page_link', models.ForeignKey(blank=True, to='cms.Page', help_text='If present, clicking on image will take user to specified page.', null=True, verbose_name='page', on_delete=models.CASCADE)),
             ],
             options={
                 'abstract': False,
@@ -28,7 +28,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Slider',
             fields=[
-                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
+                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin', on_delete=models.CASCADE)),
             ],
             options={
                 'abstract': False,
